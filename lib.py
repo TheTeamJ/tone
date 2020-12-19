a40e9b4bd2fb03c0ca771b686173d875af37d586
daiiz <da.iz.mg@gmail.com>
2020-12-19 20:48:39 +0900

    add lib

---
+++ lib.py
@@ -0,0 +1,27 @@
+import os
+# def get_dir_path(dir_name, image_name):
+#   if not dir_name or not image_name:
+#     raise Exception('dir_name and image_name are required.')
+
+# base_dir = './'
+base_dir = './tmproot/'
+
+def remove_tmp_files(image_name, thresholds):
+  if not image_name:
+    raise Exception('image_name is required.')
+
+  tmp_file_paths = []
+  thresholds.append('base')
+  for t in thresholds:
+    tmp_file_paths.append(base_dir + 'tmp/' + image_name + '.' + str(t) + '.masked.png')
+
+  for tmp_file_path in tmp_file_paths:
+    if os.path.exists(tmp_file_path):
+      os.remove(tmp_file_path)
+
+def create_dirs():
+  dir_names = ['tmp', 'raw', 'out']
+  for dir_name in dir_names:
+    dir_path = base_dir + dir_name
+    if not os.path.exists(dir_path):
+      os.makedirs(dir_path)
