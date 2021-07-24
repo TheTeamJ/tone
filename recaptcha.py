3540d2184018dcd153f9fd0bb99f82880b30d5ca
daiiz <da.iz.mg@gmail.com>
2021-07-24 17:20:48 +0900

    impl verify_recaptcha_v3

--- recaptcha.py
+++ recaptcha.py
@@ -1,4 +1,14 @@
+import os, requests
+
+recaptcha_secret_key = os.environ.get('RECAPTCHA_SECRET_KEY', None)
+
 def verify_recaptcha_v3(recaptcha_token):
   print('verify reCAPTCHA')
-  print(">>>", recaptcha_token)
-  return False
+  if not recaptcha_secret_key:
+    print('RECAPTCHA_SECRET_KEY is not found')
+    return False
+  url = f'https://www.google.com/recaptcha/api/siteverify?secret={recaptcha_secret_key}&response={recaptcha_token}'
+  data = requests.post(url).json()
+  success = data.get('success', False)
+  print('reCAPTCHA score:', data.get('score'))
+  return success
