35b5b6a182d1ae7f961d0400c916f288bebb7fd2
daiiz <da.iz.mg@gmail.com>
2021-07-25 17:09:18 +0900

    pasteLayersで処理できた

--- tone.py
+++ tone.py
@@ -3,6 +3,10 @@ import os, math, cv2
 import numpy as np
 from lib import base_dir
 
+BASE_IMAGE_MODE_W = 'White'
+BASE_IMAGE_MODE_T = 'Transparent'
+BASE_IMAGE_MODE_C = 'Color' # 原画との合成
+
 # グレースケールのみ対応
 def pil2cv (image):
   return np.array(image, dtype=np.uint8)
@@ -38,13 +42,17 @@ def pasteLayers (img_base, layer_paths, out_file_name='out.webp', save_format='w
     im = Image.open(layer_path)
     im_dst.paste(im, (0, 0), im)
   im_dst = pil2cv(im_dst)
-  im_dst = cv2.cvtColor(im_dst, cv2.COLOR_BGRA2GRAY)
 
   # 要求に応じて二値化する
   if binarization_threshold is not None:
     _, im_dst = cv2.threshold(im_dst, binarization_threshold, 255, cv2.THRESH_BINARY)
     # 背景色 (黒以外の箇所の色) の透過処理はこのタイミングで行う
+    if base_image_mode in [BASE_IMAGE_MODE_T, BASE_IMAGE_MODE_C]:
+      white = np.all(im_dst == [255, 255, 255, 255], axis=-1)
+      im_dst[white, -1] = 0
 
+  if base_image_mode == BASE_IMAGE_MODE_W:
+    im_dst = cv2.cvtColor(im_dst, cv2.COLOR_BGRA2GRAY)
   cv2.imwrite(out_file_path, im_dst)
   return out_file_path
 
